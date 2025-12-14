d5f561cd6692ce040e32be262206824f0482c96d
Farhan Ananda Muchti Harahap <farhanananda@MacBook-Air-2.local>
2025-12-14 15:05:54 +0700

    solving cors

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(
   cors({
     origin: ['http://localhost:5173',
-    'https://moneycash-nu.vercel.app/'
+    'https://moneycash-nu.vercel.app'
     ],
     credentials: false
   })
